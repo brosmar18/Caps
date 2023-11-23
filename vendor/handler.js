fce1e20ff04fb7ad8d324657de55f0ceaa2ed23f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-22 22:22:16 -0600

    driver events successful

--- vendor/handler.js
+++ vendor/handler.js
@@ -4,6 +4,7 @@ const Chance = require('chance');
 const chance = new Chance();
 const eventPool = require('../eventPool');
 
+// Create Order
 function createOrder(storeName) {
   return {
     store: storeName,
@@ -13,13 +14,14 @@ function createOrder(storeName) {
   };
 }
 
-
+// Pick Up request
 setInterval(() => {
   const order = createOrder('TheVendor'); 
   console.log('VENDOR: New pickup request', order);
   eventPool.emit('pickup', order);
-}, 8000);
+}, 5000);
 
+// Delivery response
 const handleDelivery = (payload) => {
   console.log(`VENDOR: Thanks for delivering the order: ${payload.orderId}`);
 }
