b83db0c06cd2da08b4a437e3a4eb6fcd67adde5f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-22 22:28:42 -0600

    driver tests passing

---
+++ driver/driver-handler.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { handlePickup, handleDelivered } = require('./handler');
+const eventPool = require('../eventPool');
+
+
+jest.mock('../eventPool', () => ({
+  emit: jest.fn(),
+  on: jest.fn()
+}));
+
+jest.useFakeTimers();
+
+describe('Driver Event Handlers', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should emit in-transit and log pickup message on handlePickup', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const order = { orderId: '1234' };
+
+    handlePickup(order);
+
+    expect(consoleSpy).toHaveBeenCalledWith(`DRIVER: Picked up ${order.orderId}`);
+    expect(eventPool.emit).toHaveBeenCalledWith('in-transit', order);
+
+    
+    jest.advanceTimersByTime(3000);
+
+    
+    expect(eventPool.emit).toHaveBeenCalledWith('delivered', order);
+  });
+
+  it('should emit delivered and log delivery message on handleDelivered', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    const order = { orderId: '5678' };
+
+    handleDelivered(order);
+
+    expect(consoleSpy).toHaveBeenCalledWith(`DRIVER: Delivered ${order.orderId}`);
+    expect(eventPool.emit).toHaveBeenCalledWith('delivered', order);
+  });
+});
