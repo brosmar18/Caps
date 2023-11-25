38cfac3b13205c96f30378d788fe1225320ab430
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-24 22:51:44 -0600

    interactions updated

--- server/index.js
+++ server/index.js
@@ -1,36 +1,35 @@
-;'use strict';
-
-
+'use strict';
 
 const { Server } = require('socket.io');
-
 require('dotenv').config();
 const PORT = process.env.PORT || 5002;
-
 const server = new Server();
-
 const caps = server.of('/caps');
 
 caps.on('connection', (socket) => {
     console.log(`Client Connected: ${socket.id}`);
 
     socket.on('join', (room) => {
-        console.log(`Socket ${socket.id} joining ${room}`);
+        console.log(`Socket ${socket.id} joined room ${room}`);
         socket.join(room);
     });
 
     socket.on('pickup', (payload) => {
-        console.log('Event:', { event: 'pickup', payload });
-        socket.broadcast.emit('pickup', payload); // Broadcast to all clients except sender
-      });
-    
-      socket.on('in-transit', (payload) => {
-        socket.to(payload.vendorId).emit('in-transit', payload); // Emit to specific vendor room
-      });
-    
-      socket.on('delivered', (payload) => {
-        socket.to(payload.vendorId).emit('delivered', payload); // Emit to specific vendor room
-      });
+        console.log(`New Pickup Request Received: Order ID ${payload.orderId}`);
+        socket.broadcast.emit('pickup', payload);
+    });
+
+    socket.on('in-transit', (payload) => {
+        console.log(`Order In-Transit: Order ID ${payload.orderId}`);
+        socket.to(payload.store).emit('in-transit', payload);
+    });
+
+    socket.on('delivered', (payload) => {
+        console.log(`Order Delivered: Order ID ${payload.orderId}`);
+        socket.to(payload.store).emit('delivered', payload);
+    });
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
